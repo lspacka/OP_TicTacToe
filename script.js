86568270ca9bcdaee35e2a5a52a71c013d30283a
julian <jdrc@protonmail.com>
2024-02-20 21:18:43 -0500

    functional winning check

--- script.js
+++ script.js
@@ -21,18 +21,24 @@ function KeyFunc(e) {
 // }
 
 function GameBoard(board) {
-    // board = JSON.parse(JSON.stringify(board))
+    const board2 = JSON.parse(JSON.stringify(board))
 
     const add = (input, play) => {
         const [row, col] = input.split(',')
-        if (board[row-1][col-1] == '-') {
-            board[row-1][col-1] = play
+        if (board2[row-1][col-1] == '-') {
+            board2[row-1][col-1] = play
             return true
         } 
     }
 
+    const add2 = (square, play) => {
+        if (square.textContent) return
+        square.textContent = play
+        board2[square.id-1] = play
+    }
+
     const checkWin = () => {
-        const flat = [].concat.apply([], board)   // flattens 2d board array
+        const flat= [].concat.apply([], board2)   // flattens 2d board2 array
         const lines = [
             [0, 1, 2],
             [3, 4, 5],
@@ -46,8 +52,7 @@ function GameBoard(board) {
 
         for (let i = 0; i < lines.length; i++) {
             const [a, b, c] = lines[i]
-            if (flat[a] && flat[a]==flat[b] && flat[a]==flat[c] &&
-                flat[a]!='-' && flat[b]!='-' && flat[c]!='-') {
+            if (flat[a] && flat[a]==flat[b] && flat[a]==flat[c]) {
                 return flat[a]
             }
         }
@@ -55,23 +60,23 @@ function GameBoard(board) {
     } 
     
     const show = () => {
-        for (let i = 0; i < board.length; i++) {
-            let output = board.map(innerArray => innerArray.join(' '))
+        for (let i = 0; i < board2.length; i++) {
+            let output = board2.map(innerArray => innerArray.join(' '))
             output = output.join('\n')
             console.log(output)
         }
     }
 
     const checkDraw = () => {
-        const flat = [].concat.apply([], board)
+        const flat = [].concat.apply([], board2)
         return flat.every(square => square != '-')
     }
 
-    return { add,  checkWin, checkDraw, show }
+    return { add, add2, checkWin, checkDraw, show }
 }
 
 function Game() {
-    const new_grBoard = Array(9).fill(' ') // for storing the moves on the screen
+    const new_grBoard = Array(9).fill('') // for storing the moves on the screen
     let play = 'X'
     // let player1 = createPlayer('X')
     // let player2 = createPlayer('O')
@@ -92,20 +97,55 @@ function Game() {
         console.clear()
     }
 
+    function checkWinner(board) {
+        winner = board.checkWin()
+        console.log(winner)
+        if (winner) {
+            console.log(`winner: ${winner}`)
+            banner.textContent = `${play} WINS`
+        }
+    }
+
     const loop = () => {
         banner.textContent = `Next player: ${play}`
-        winner = gr_board.checkWin()
+        // if (winner) {
+        //     console.log(`winner: ${winner}`)
+        //     banner.textContent = `${play} WINS`
+        // }
 
         board.forEach(square => {
             square.addEventListener('click', () => {
-                square.textContent = play
-                gr_board[square.id-1] = play
-                console.log(gr_board)
+                gr_board.add2(square, play)
+                checkWinner(gr_board)
+                // winner = gr_board.checkWin()
+                // console.log(winner)
+                // if (winner) {
+                //     console.log(`winner: ${winner}`)
+                //     banner.textContent = `${play} WINS`
+                // }
+                // console.log(gr_board)
                 play = (play == 'X') ? 'O' : 'X'
                 banner.textContent = `Next player: ${play}`
             })
         })
         
+        // while (true) {
+        //     winner = gr_board.checkWin()
+        //     if (winner) {
+        //         banner.textContent = `${play} WINS!`
+        //         break
+        //     }
+
+        //     board.forEach(square => {
+        //         square.addEventListener('click', () => {
+        //             square.textContent = play
+        //             gr_board[square.id-1] = play
+        //             console.log(gr_board)
+        //             play = (play == 'X') ? 'O' : 'X'
+        //             banner.textContent = `Next player: ${play}`
+        //         })
+        //     }) 
+        // }
     }
 
     // const loop = () => {
